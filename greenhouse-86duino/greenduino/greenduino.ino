fceccd188505ac74287be11b6630176a887ecdc5
Jean-Christophe MELIKIAN <jean.melikian@gmail.com>
2017-06-30 09:13:16 +0200

    Using the YL-69 & YL-38 to detect the soil humidity

---
+++ greenhouse-86duino/greenduino/greenduino.ino
@@ -0,0 +1,25 @@
+void setup() {
+  Serial.begin(9600);
+  pinMode(A0, INPUT);
+}
+
+
+void loop() {
+
+  int SensorValue = analogRead(A0);
+  Serial.print(SensorValue); Serial.print(" - ");
+  
+  if(SensorValue >= 1000) {
+   Serial.println("Sensor is not in the Soil or DISCONNECTED");
+  }
+  if(SensorValue < 1000 && SensorValue >= 600) { 
+   Serial.println("Soil is DRY");
+  }
+  if(SensorValue < 600 && SensorValue >= 370) {
+   Serial.println("Soil is HUMID"); 
+  }
+  if(SensorValue < 370) {
+   Serial.println("Sensor in WATER");
+  }
+  delay(50);
+}
