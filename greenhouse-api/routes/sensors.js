b4ef456633ea159cee5baa849fad7d3b07de7d42
Jean-Christophe MELIKIAN <jean.melikian@gmail.com>
2017-07-03 21:44:22 +0200

    Sensors route

--- greenhouse-api/routes/sensors.js
+++ greenhouse-api/routes/sensors.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var bodyParser = require('body-parser');
-
+var moment = require('moment');
 var jsonParser = bodyParser.json();
 
 /* GET users listing. */
@@ -9,6 +9,7 @@ router.get('/', function (req, res, next) {
 	res.send('respond with a resource');
 }).post('/', jsonParser, function (req, res, next) {
 	if (!req.body) return res.sendStatus(400);
+	req.body['timestamp'] = moment(new Date()).format("YYYY-MM-DD HH:mm:ss.SSS");
 	console.log(req.body);
 	return res.sendStatus(201);
 });
